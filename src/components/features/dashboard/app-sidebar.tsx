3525410b1483096a2bcb787782b9204a934aad9c
AdeKurniawannnn <adekurniawankebi2@gmail.com>
2025-07-17 16:16:37 +0700

    Navigasi dan sidebar

--- src/components/features/dashboard/app-sidebar.tsx
+++ src/components/features/dashboard/app-sidebar.tsx
@@ -38,11 +38,11 @@ import {
 const data = {
 
   navMain: [
-    {
-      title: "Dashboard",
-      url: "/dashboard",
-      icon: LayoutDashboardIcon,
-    },
+    // {
+    //   title: "Dashboard",
+    //   url: "/dashboard",
+    //   icon: LayoutDashboardIcon,
+    // },
     {
       title: "Orion",
       url: "/orion",
@@ -120,21 +120,21 @@ const data = {
     },
   ],
   documents: [
-    {
-      name: "Client Database",
-      url: "#",
-      icon: DatabaseIcon,
-    },
-    {
-      name: "Performance Reports",
-      url: "#",
-      icon: ClipboardListIcon,
-    },
-    {
-      name: "Brand Guidelines",
-      url: "#",
-      icon: FileIcon,
-    },
+    // {
+    //   name: "Client Database",
+    //   url: "#",
+    //   icon: DatabaseIcon,
+    // },
+    // {
+    //   name: "Performance Reports",
+    //   url: "#",
+    //   icon: ClipboardListIcon,
+    // },
+    // {
+    //   name: "Brand Guidelines",
+    //   url: "#",
+    //   icon: FileIcon,
+    // },
   ],
 }
 
@@ -158,7 +158,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       </SidebarHeader>
       <SidebarContent>
         <NavMain items={data.navMain} />
-        <NavDocuments items={data.documents} />
+        {/* <NavDocuments items={data.documents} /> */}
         <NavSecondary items={data.navSecondary} className="mt-auto" />
       </SidebarContent>
     </Sidebar>
